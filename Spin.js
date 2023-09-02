51a7ae7a69db22697efa241d4b84e75fe8c1135c
Sadiq <113123466+sadiqalhumood@users.noreply.github.com>
2023-09-02 00:01:33 -0400

    13:22 Created the deposit simple function only

---
+++ Spin.js
@@ -0,0 +1,15 @@
+// 1. Player deposit money
+// 2. Determine number of lines to bet
+// 3. Collect a bet amount
+// 4. Spin 
+// 5. Check if the user wins
+//6. Give the user their winnnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
